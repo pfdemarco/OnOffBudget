8e6eaab05d6c3f56bd1f23b2c094357603967f36
Patrick DeMarco <pfdemarco@hotmail.com>
2021-04-24 10:13:08 -0400

    added proc env and http and ssl stuff

--- Develop/server.js
+++ Develop/server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+var http = require('http');
+var enforce = require('express-sslify');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
@@ -23,6 +25,6 @@ mongoose.connect("mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
+http.createServer(app).listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
